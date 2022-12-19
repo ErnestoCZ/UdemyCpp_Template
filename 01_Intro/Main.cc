fb42233df2ae0720e9fc33464034d0b984da8cbf
Ernest Jerzy Czechowski <ernest.czechowski@gmail.com>
2022-12-19 01:12:22 +0100

    ArrayPointer Exercise1 task finished. Bug has to be fixed

--- 01_Intro/Main.cc
+++ 01_Intro/Main.cc
@@ -1,10 +1,36 @@
 #include <iostream>
+#include "cstddef"
 
 
+    double array_sum(const double *p_array, const std::uint32_t &length)
+    {
+    std::uint32_t counter = 0;
+    double sum = 0;
+
+    for(std::int32_t i = 0; i < length; i++){
+        sum = sum + p_array[i];
+    }
+
+    return sum;
+    };
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    // Exercise 1
+    const std::uint32_t array_length = 100;
+    double sum = 0.0;
+
+    double *p_array1 = nullptr;
+    p_array1 = new double[array_length];    //allocate an array on heap
+
+    for (std::uint32_t i = 0; i < 100; i++)
+    {
+        p_array1[i] = i; //automatical direct converting through compiler base address and additional offset
+    };
+
+    std::cout << array_sum(p_array1,array_length) << std::endl;
+
+
 
     return 0;
 }
--- 03_ArrayPointer/_Exercise1/Exercise/exercise.cc
+++ 03_ArrayPointer/_Exercise1/Exercise/exercise.cc
@@ -6,9 +6,27 @@
 // Exercise 1
 double array_sum(const double *p_array, const std::uint32_t &length)
 {
+    double sum = 0;
+
+    for(std::uint32_t i = 0; i < length; i++){
+        sum = sum + p_array[i];
+    }
+
+    return sum;
 }
 
 // Exercise 2
 int *array_constructor(const int &value, const std::uint32_t &length)
 {
+    //TODO Fix : function creates a one element to big array
+    //create an array on heap
+    int* arr = new int[length];
+
+    //init array with passed value
+    for (std::uint32_t i = 0; i < length; ++i)
+    {
+        arr[i] = value;
+    }
+
+    return arr;
 }
