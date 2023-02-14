cd0b9ff646aded52a61ee25ef778b48a021e0f94
Ernest Jerzy Czechowski <ernest.czechowski@gmail.com>
2023-02-14 22:07:45 +0100

    morebasic2 finished

--- 04_MoreBasics/_Exercise2/Exercise/exercise.cc
+++ 04_MoreBasics/_Exercise2/Exercise/exercise.cc
@@ -1,3 +1,9 @@
 #include <iostream>
 
 #include "exercise.h"
+
+
+void printPlayerData(const PlayerData &player){
+
+    std::cout << "Pos: " << "( " << player.x_pos << " , " << player.y_pos << " )";
+}
